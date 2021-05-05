7c59540b610a72dc06e8dc12eaa67f51269243cb
Muneeb Ahmad <muneeb.panwar@gmail.com>
2021-05-05 15:10:50 +0500

    Added agency user

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,8 @@
 Rails.application.routes.draw do
   root 'homes#index'
+  devise_for :agencies, :skip => [:registrations]
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  authenticated :agency_user do
+    root 'admin/homes#index', as: :agency_user
+  end
 end
