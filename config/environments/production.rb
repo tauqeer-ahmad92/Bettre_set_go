0ef6390d7b4da711688c3f606e52be18f9787af6
Muneeb Ahmad <muneeb.panwar@gmail.com>
2021-05-07 14:45:17 +0500

    bug fixed

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -117,12 +117,12 @@ Rails.application.configure do
   # config.active_record.database_selector = { delay: 2.seconds }
   # config.active_record.database_resolver = ActiveRecord::Middleware::DatabaseSelector::Resolver
   # config.active_record.database_resolver_context = ActiveRecord::Middleware::DatabaseSelector::Resolver::Session
-  config.action_mailer.default_url_options = { host: 'better-set-go.herokuapp.com/' }
+  config.action_mailer.default_url_options = { host: 'better-set-go.herokuapp.com' }
   config.action_mailer.delivery_method = :smtp
   config.action_mailer.smtp_settings = {
     :user_name => ENV['SENDGRID_USERNAME'],
     :password => ENV['SENDGRID_PASSWORD'],
-    :domain => 'better-set-go.herokuapp.com/',
+    :domain => 'better-set-go.herokuapp.com',
     :address => 'smtp.sendgrid.net',
     :port => 587,
     :authentication => :plain,
