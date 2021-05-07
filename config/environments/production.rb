6c968207390a27184b0811b87b53a6ab704532bf
Muneeb Ahmad <muneeb.panwar@gmail.com>
2021-05-07 13:21:19 +0500

    Added sendgrid

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -117,4 +117,26 @@ Rails.application.configure do
   # config.active_record.database_selector = { delay: 2.seconds }
   # config.active_record.database_resolver = ActiveRecord::Middleware::DatabaseSelector::Resolver
   # config.active_record.database_resolver_context = ActiveRecord::Middleware::DatabaseSelector::Resolver::Session
+  
+
+  # config.action_mailer.default_url_options = { host: 'better-set-go.herokuapp.com/' }
+  # config.action_mailer.delivery_method = :smtp
+  # config.action_mailer.smtp_settings = {
+  #   :user_name => ENV['SENDGRID_USERNAME'],
+  #   :password => ENV['SENDGRID_PASSWORD'],
+  #   :domain => 'better-set-go.herokuapp.com/',
+  #   :address => 'smtp.sendgrid.net',
+  #   :port => 587,
+  #   :authentication => :plain,
+  #   :enable_starttls_auto => true
+  # }
+
+  ActionMailer::Base.smtp_settings = {
+  domain: 'better-set-go.herokuapp.com/',
+  address:        "smtp.sendgrid.net",
+  port:            587,
+  authentication: :plain,
+  user_name:      'apikey',
+  password:       ENV['SENDGRID_API_KEY']
+}
 end
